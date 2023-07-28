571a4a82f9e6b281d49aedf6a9dc134f0e510fbd
= <=>
2023-07-28 08:51:46 +0530

    Added dark and light mode to Project-1

--- Project-1/src/App.jsx
+++ Project-1/src/App.jsx
@@ -1,11 +1,19 @@
+import React from 'react';
+
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(prevMode => !prevMode);
+  }
+
   return (
-    <div className="container">
-      <Header />
+    <div className={`container ${darkMode ? "dark" : ""}`}>
+      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Main />
       <Footer />
     </div>
--- Project-1/src/components/Header.jsx
+++ Project-1/src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-function Header() {
+function Header({ darkMode, toggleDarkMode }) {
   return (
     <div>
       <nav className="navbar">
           <img src="/public/logo.png" alt="Logo" className="navbar__logo"/>
-          <h3 className="navbar__heading">React Course - Project 1</h3>
+          {/* <h3 className="navbar__heading">React Course - Project 1</h3> */}
+
+          <div className="mode__container">
+            <div className={"mode__dark " + (darkMode ? "dark" : "")}>Dark</div>
+            <div className={`dark__mode__slider ${ darkMode ? "dark" : ""}`} onClick={toggleDarkMode}></div>
+            <div className={"mode__light " + (darkMode ? "" : "dark")}>Light</div>
+          </div>
       </nav>
     </div>
   );
